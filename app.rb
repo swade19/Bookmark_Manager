67c8268fbb49b3000be76b208fa16843b4689118
Student <student@Admins-MacBook-Air.local>
2019-11-04 14:47:21 +0000

    Created first feature test

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require "sinatra/base"
 
 class BookmarkManager < Sinatra::Base
   get "/" do
-    "Hello World"
+    "Bookmark Manager"
+  end
+
+  get "/bookmarks" do
+    "Bookmarks go here"
   end
 
   run! if app_file == $0
--- spec/features/ viewing_bookmarks_spec.rb
+++ spec/features/ viewing_bookmarks_spec.rb
@@ -5,4 +5,9 @@ feature "Viewing Bookmarks" do
     visit ("/")
     expect(page).to have_content "Bookmark Manager"
   end
+
+  scenario "allows you to view the bookmarks" do
+    visit ("/bookmarks")
+    expect(page).to have_content "https://www.google.com"
+  end
 end
